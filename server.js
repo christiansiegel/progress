6ca5020977eb8ba817aedc2e540262a26e57c014
Christian Siegel <christian.siegel@gmx.net>
2019-11-05 22:19:28 +0100

    working front and simple backend

---
+++ server.js
@@ -0,0 +1,35 @@
+const http = require("http");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+const html = fs.readFileSync("index.html", "utf8");
+const favicon = fs.readFileSync("favicon.ico");
+
+const rand = (max) => {
+  return Math.floor(Math.random() * (max * 1));
+}
+
+http.createServer().on("request", async (req, res) => {
+  if (req.url === "/api") {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon");
+    const pokemon = await response.json();
+    const items = pokemon.results
+      .map(p => ({
+        percent: rand(100),
+        maxPercent: rand(100),
+        name: p.name
+      }))
+      .map(p => ({
+        ...p,
+        progressText: p.percent + " / " + p.maxPercent
+      }));
+    const groups = [items, [items[0]]];
+    console.log(items);
+    res.end(JSON.stringify(groups));
+  } else if (req.url === "/favicon.ico") {
+    res.end(favicon);
+  } else {
+    //res.end(html);
+    res.end(fs.readFileSync("index.html", "utf8"));
+  }
+}).listen(3333);
